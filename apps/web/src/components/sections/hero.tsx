e792e67b8e62a5ee1948f77e8718e39ab4750dab
Nathan <viniciosnathan@gmail.com>
2025-12-20 18:31:42 -0300

    feat: enhance Hero component with motion animations for improved visual appeal

--- apps/web/src/components/sections/hero.tsx
+++ apps/web/src/components/sections/hero.tsx
@@ -1,3 +1,4 @@
+import { motion } from "motion/react";
 import { Bebas_Neue } from "next/font/google";
 
 const bebasNeue = Bebas_Neue({
@@ -9,13 +10,24 @@ export default function Hero() {
 	return (
 		<div className="flex h-dvh flex-row items-center justify-center">
 			<div className="flex flex-row items-center gap-2">
-				<div className="h-0.5 w-100 bg-linear-to-l from-white to-transparent" />
-				<div
-					className={`${bebasNeue.className} font-bold text-8xl tracking-tight`}
+                <motion.div className="h-0.5 w-100 bg-linear-to-l from-white to-transparent"
+                initial={{ width: 0 }}
+                animate={{ width: "400px" }}
+                transition={{ duration: 2 }}
+                />
+				<motion.div
+                    className={`${bebasNeue.className} font-bold text-8xl tracking-tight`}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5 }}
 				>
 					<span className="text-[110px]">H</span>irion
-				</div>
-                <div className="h-0.5 w-100 bg-linear-to-r from-white to-transparent" />
+				</motion.div>
+                <motion.div className="h-0.5 w-100 bg-linear-to-r from-white to-transparent"
+                initial={{ width: 0 }}
+                animate={{ width: "400px" }}
+                transition={{ duration: 2 }}
+                />
 			</div>
 		</div>
 	);
