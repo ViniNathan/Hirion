527578e6fb585e187ed6b39725c408d4c89ca1f2
Nathan <viniciosnathan@gmail.com>
2025-12-19 22:47:44 -0300

    feat: enhance prompts by implementing coverLetterCreator and updating curriculumScorer to use SystemMessage

--- packages/ai/prompts/index.ts
+++ packages/ai/prompts/index.ts
@@ -1,5 +1,8 @@
+import { SystemMessage } from "@langchain/core/messages";
+
 const prompts = {
-	curriculumScorer: `
+	curriculumScorer: new SystemMessage({
+		content: `
     Você é uma recrutador ATS (Applicant Tracking System) que avalia currículos de candidatos para uma determinada vaga.
     Você recebe um currículo e a descrição da vaga e deve gerar um score de 0 a 100 para o candidato com base nessas informações.
     O score deve ser baseado em:
@@ -24,8 +27,23 @@ const prompts = {
             "O candidato não possui os conhecimentos como Git, Scrum, entre outras metodologias de desenvolvimento de software que são necessárias para a vaga."
             "O candidato não possui os conhecimentos como TypeScript, React, Node.js, que são necessárias para a vaga."
         ]
-    }
+    }`,
+	}),
+    coverLetterCreator: new SystemMessage({
+        content: `
+        Você é um criador de cartas de apresentação de candidatos para uma determinada vaga.
+        Você recebe um currículo e a descrição da vaga e deve gerar uma carta de apresentação para o candidato com base nessas informações.
+        A carta de apresentação deve ser baseada em:
+        - Busca por palavras chaves no currículo e na descrição da vaga
+        - Experiência do candidato em relação às habilidades necessárias para a vaga
+        - Qualificações do candidato em relação às habilidades necessárias para a vaga
+
+        Considere o seguinte exemplo de resposta (você não deve seguir exatamente este exemplo, mas deve seguir o mesmo formato JSON):
+        {
+            "coverLetter": "Olá, sou [Nome do candidato], um [Profissão] com [experiência] anos de experiência em [Habilidades]. Sou um profissional [Profissão] com [experiência] anos de experiência em [Habilidades]. Sou um profissional [Profissão] com [experiência] anos de experiência em [Habilidades]."
+        }
     `,
+    }),
 };
 
 export { prompts };
