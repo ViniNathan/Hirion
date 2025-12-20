c345ce73686a9ccefcb0f49f14b9e41ae61dbe18
Nathan <viniciosnathan@gmail.com>
2025-12-20 18:48:39 -0300

    style: increase max-width of container in Home page for improved layout

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -4,7 +4,7 @@ import Hero from "@/components/sections/hero";
 export default function Home() {
 
 	return (
-		<div className="container mx-auto max-w-3xl px-4 py-2">
+		<div className="container mx-auto max-w-5xl px-4 py-2">
 			<Hero />
 		</div>
 	);
